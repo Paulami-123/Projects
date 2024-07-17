4e02de0450d597182987e49a20a45185c6944e4d
Paulami-123 <paulamiibanerjee@gmail.com>
2024-07-17 14:23:37 +0530

    Created a test api route

--- Blog/api/index.js
+++ Blog/api/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js"
 
 dotenv.config()
 
@@ -12,6 +13,8 @@ mongoose.connect(process.env.MONGODB_URL)
     console.log(err);
 })
 const app = express();
+
+app.use('/api/user', userRouter)
 
 app.listen(3000, ()=> {
     console.log("Listening at PORT 3000 🎧")
