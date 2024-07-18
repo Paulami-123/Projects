df3d00e3f23a83afecb9ef8a4d6205e8a0611ca3
Paulami-123 <paulamiibanerjee@gmail.com>
2024-07-18 14:35:20 +0530

    Added error handler middleware

--- Blog/api/controllers/auth.controller.js
+++ Blog/api/controllers/auth.controller.js
@@ -1,12 +1,12 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js"
 
-export const signup = async(req, res) => {
+export const signup = async(req, res, next) => {
     const { username, email, password } = req.body
     if(!username || !email || !password || username==="" || email==="" || password===""){
-        return res.status(400).json({
-            error: "All fields are required"
-        })
+        next(errorHandler(400, 'All fields are required'));
+        res.status(400).json("All fields are required")
     }
     const hashedPassword = bcryptjs.hashSync(password, 10)
     const newUser = new User({
@@ -21,6 +21,6 @@ export const signup = async(req, res) => {
         })
     }
     catch(error){
-        res.status(500).json(error)
+        next(error)
     }
 }
