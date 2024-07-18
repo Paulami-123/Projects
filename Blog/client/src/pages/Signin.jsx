c8c3f931e5ecc771e1e455a505c8c09ecea53d5a
Paulami-123 <paulamiibanerjee@gmail.com>
2024-07-18 20:12:07 +0530

    Sign in route api created

--- Blog/client/src/pages/Signin.jsx
+++ Blog/client/src/pages/Signin.jsx
@@ -2,7 +2,52 @@ import React from 'react'
 
 const Signin = () => {
   return (
-    <div>Signin</div>
+    <div className='min-h-screen mt-20'>
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left side */}
+        <div className="flex-1">
+          <Link to="/" className="text-4xl font-bold dark:text-white">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Paulami's
+            </span>
+            Blog
+          </Link>
+          <p className='text-md mt-5 font-semibold'>
+            This is a demo project. You can sign up with your email & password or with Google.
+          </p>
+        </div>
+        {/* right side */}
+        <div className="flex-1">
+          <form className='flex flex-col gap-4'>
+            <div>
+              <Label value='Your email' />
+              <TextInput type='email' placeholder='name@company.com' id='email' />
+            </div>
+            <div>
+              <Label value='Password' />
+              <TextInput type='password' placeholder='Password' id='password' />
+            </div>
+            <Button gradientDuoTone={'purpleToPink'} type='submit'> Sign In
+              {/* {loading ?( 
+              <>
+                <Spinner size={'sm'} />
+                <span className='pl-3'>Loading...</span>
+              </>
+              ) : (
+                'Sign In'
+              )} */}
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Already have an account?</span>
+            <Link to={'/signin'} className='text-blue-500'> Sign In</Link>
+          </div>
+          {errorMessage && (
+            <Alert className='mt-5' color={'failure'}>{errorMessage}</Alert>
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
 
