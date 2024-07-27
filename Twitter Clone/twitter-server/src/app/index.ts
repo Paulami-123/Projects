6b40a09034ce3ec7e593500d248a53eb2e0f88cf
Paulami-123 <paulamiibanerjee@gmail.com>
2024-07-27 20:19:50 +0530

    Prisma ORM setup

--- Twitter Clone/twitter-server/src/app/index.ts
+++ Twitter Clone/twitter-server/src/app/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import bodyParser from 'body-parser'
+import { prismaClient } from '../client/db';
 
 export async function initServer(){
     const app = express();
